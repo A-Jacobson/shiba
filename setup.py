aaaebbcddbf27fefe6fdacf3989d93b2ff7ee928
A-Jacobson <jacobsonaustinj@gmail.com>
2019-05-09 03:32:32 -0700

    requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='shiba',
     version='0.0.2',
-    description='PyTorch trainer, datasets, criterion, and utility functions',
+    description='PyTorch trainer',
     url='https://github.com/A-Jacobson/shiba',
     license='MIT',
     python_requires='>=3',
